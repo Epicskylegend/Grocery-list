65bfef7bda6d3d7176af84bed1cc1729a3a0e422
Epicskylegend <adeba003@gmail.com>
2023-05-18 23:53:24 -0500

    bug fixes

--- GroceryList.js
+++ GroceryList.js
@@ -1,33 +1,32 @@
-$(function() {
-    var callback = function(event) {
-        event.preventDeault();
-        var input = $('input[type=text][name=item]')
-        value = input.val(),
-        need = ($(event.target).attr('id') === 'addNeed'),
-        item = $('<li><input type = "checkbox" name="item"> ' + value + ' <a href="#">remove</a></li>'),
-        list = (need) ? $('ul').first() : $('ul').last();
+(function() {
+	var callback = function(event) {
+		event.preventDefault();
+		var input = $('input[type=text][name=item]'),
+			value = input.val(),
+			need = ($(event.target).attr('id') === 'addNeed'),
+			item = $('<li><input type="checkbox" name="item"> ' + value + ' <a href="#">remove</a></li>'),
+			list = (need) ? $('ul').first() : $('ul').last();
+		
+		input.val("");
+		input.focus();
 
-        input.val("");
-        input.focus();
+		if (value === "") return;
 
-        if(value === "") return;
+		if (!need) {
+			item.find('input').attr('checked', true);
+		}
+		item.appendTo(list);
+	}
 
-        if (!need) {
-            item.find('input').attr('checked', true);
-        }
-        item.appendTo(list);
-    }
+	$('#addHave, #addNeed').click(callback);
+	
+	$('ul').on('click', 'li a', function(event){
+		$(event.target).parent('li').remove();
+	});
 
-    $('#addHave, #addNeed').click(callback);
-
-    $('ul').on('click', 'li a', function(event) {
-            $(event.target).parent('li').remove();
-    });
-
-    $('ul').on('click', 'input[type=checkbox]', function(event) {
-        var listItem = $(event.target).parent('li'),
-        list = (event.target.checked) ? $('ul').last() : $('ul').first();
-        listItem.appendTo(list);
-    });
-    
+	$('ul').on('click', 'input[type=checkbox]', function(event){
+		var listItem = $(event.target).parent('li'),
+			list = (event.target.checked) ? $('ul').last() : $('ul').first();
+		listItem.appendTo(list);
+	});
 });
